c4d348c3d0dccc7288e18817db115c03ad03b954
Sandeep Battula <sandeep.battula@nagarro.com>
2025-12-09 16:15:26 +0530

    WIP updates

--- services/README.md
+++ services/README.md
@@ -1,1 +1,7 @@
 ## TODO Add Content
+
+## To Run in dev mode
+
+```bash
+uv run fastmcp dev main.py:mcp
+```
--- services/mcps/main.py
+++ services/mcps/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from mcps!")
+from fastmcp import FastMCP
+from cas.run import mcp as cas_mcp
 
+mcp = FastMCP()
+mcp.mount("cas", cas_mcp)
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
